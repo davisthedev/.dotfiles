bead1c14b9c1075344f65f9afe9e6b84b88b9c6e
Davis Sanders <davisrsanders@gmail.com>
2023-12-19 12:59:46 -0500

    init

---
+++ lvim/.config/lvim/config.lua
@@ -0,0 +1,14 @@
+require("user.plugins").config()
+
+lvim.colorscheme = "catppuccin"
+lvim.builtin.lualine.options.theme = "catppuccin"
+lvim.builtin.nvimtree.setup.update_focused_file.update_root = false
+lvim.builtin.nvimtree.setup.prefer_startup_root = true
+lvim.builtin.nvimtree.setup.sync_root_with_cwd = false
+lvim.builtin.nvimtree.setup.actions.change_dir.enable = false
+
+-- lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+-- lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
+lvim.keys.insert_mode["jk"] = "<ESC>"
+lvim.keys.visual_mode["J"] = ":m '>+1<CR>gv=gv"
+lvim.keys.visual_mode["K"] = ":m '<-2<CR>gv=gv"
